6559f62f538135af6cbcef1cf3b44110bb70959c
Lensatom <ayomidetommiwa@gmail.com>
2025-04-15 05:45:26 +0100

    :construction: feat: basic pregerences UI ready

--- src/screens/auth/components/Preferences.tsx
+++ src/screens/auth/components/Preferences.tsx
@@ -1,11 +1,14 @@
 import { MultiSelect, SingleSelect, Title } from '@/components/inc'
 import React from 'react'
-import { Text } from 'tamagui'
+import { Text, View } from 'tamagui'
 import { YStack } from 'tamagui'
+import MultiSlider from '@ptomasroos/react-native-multi-slider'
+import { PADDING_X, SCREEN_WIDTH } from '@/constants'
+import { Platform } from 'react-native'
 
 function Preferences({data, changeData}:any) {
   return (
-    <YStack gap="$4">
+    <YStack gap="$6">
       <Title text="Preferences" />
       <YStack gap="$3">
         <Text fow="500">What is your gender?</Text>
@@ -15,8 +18,34 @@ function Preferences({data, changeData}:any) {
         <Text fow="500">Who are you looking for?</Text>
         <MultiSelect opts={["Male", "Female"]} onChange={(value:string[]) => changeData("gender_pref", value)} />
       </YStack>
-      <YStack>
-        <Text fow="500">What age range are you interested in?</Text>
+      <YStack flex={1} ai="center" gap="$2">
+        <Text fow="500" w="$full">What age range are you interested in?</Text>
+        <MultiSlider
+          values={[18, 65]}
+          sliderLength={SCREEN_WIDTH - (PADDING_X * 2) - 30}
+          selectedStyle={{backgroundColor:'#CD5808', borderRadius: 99}}
+          unselectedStyle={{backgroundColor:"#E2E8F0", borderRadius: 99}}
+          containerStyle={{alignSelf:'center'}}
+          // onValuesChange={handleDistanceChange}
+          markerStyle={{
+            ...Platform.select({
+              android: {
+                height: 25,
+                width: 25,
+                borderRadius: 50,
+                backgroundColor: 'white',
+                elevation: 5
+              }
+            })
+          }}
+          trackStyle={{
+            height: 6
+          }}
+          min={18}
+          max={65}
+          step={1}
+          enableLabel
+        />
       </YStack>
     </YStack>
   )
